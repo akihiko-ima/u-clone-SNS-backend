738d9fbe4aab025ae3bbb6e791d8a502ed807df4
akihiko-ima <sr.cb.ape@gmail.com>
2024-06-07 00:44:55 +0900

    modifiy: alllow cors site

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const usersRoute = require("./routers/users")
 
 
 // expressでjson-setting
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: process.env.ALLOW_ACCESS_ORIGIN, //アクセス許可するオリジン
+  credentials: true,                       //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+  optionsSuccessStatus: 200                //レスポンスstatusを200に設定
+}))
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
